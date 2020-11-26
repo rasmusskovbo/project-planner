2a545cb5bf80daf6a0c933090168c7eac1e75538
Rasmus Skovbo <rasmusskovbo.dk@gmail.com>
2020-11-26 20:04:44 +0100

    Initial commit

---
+++ src/main/java/keastudents/projectplanner/ProjectplannerApplication.java
@@ -0,0 +1,13 @@
+package keastudents.projectplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectplannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/keastudents/projectplanner/ProjectplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package keastudents.projectplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectplannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
