1733ddd4cd15e98c7fac844cbf3e6fe4b7074e9f
Orniliya <ornx0026@stud.kea.dk>
2020-11-30 15:29:38 +0100

    frontpage and login pages

--- src/main/java/keastudents/projectplanner/Controller/UserWebController.java
+++ src/main/java/keastudents/projectplanner/Controller/UserWebController.java
@@ -5,7 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserWebController {
+
+    //show welcome-/frontpage
     @GetMapping("/")
+    public String frontpage() {
+        return "frontpage";
+    }
+
+    //show log in page
+    @GetMapping("/logIn")
+    public String logIn() {
+        return "logIn";
+    }
+
+    //show sign up page
+    @GetMapping("/signUp")
+    public String signUp() {
+        return "signUp";
+    }
+
+    @GetMapping("/overview")
     public String overview() {
         return "overview";
     }
