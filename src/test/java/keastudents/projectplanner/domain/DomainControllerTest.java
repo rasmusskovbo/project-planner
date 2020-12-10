8cc30ec03d9943773f60189f0bd0cc37d98a296c
WafaaAjan <ou_ajan@hotmail.com>
2020-12-10 15:41:33 +0100

    test-implementering

--- src/test/java/keastudents/projectplanner/domain/DomainControllerTest.java
+++ src/test/java/keastudents/projectplanner/domain/DomainControllerTest.java
@@ -1,17 +1,34 @@
 package keastudents.projectplanner.domain;
 
+import keastudents.projectplanner.data.UserMapper;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
 class DomainControllerTest {
 
     @Test
     public void createUser_returnsCorrectUser() {
+/*
+            //  create mock
+            UserMapper test = mock(UserMapper.class);
+
+            // define return value for method getUniqueId()
+          // when(test.getUniqueId()).thenReturn(43);
+
+          when(test.getUser(1)).thenReturn();
+
+            // use mock in test....
+            assertEquals(test.getUniqueId(), 43);
+        }
+
         // Instancing
 
         //  Variabler
 
 
         // Test conditions
-    }
 
+
+ */
+    }
 }
---
+++ src/test/java/keastudents/projectplanner/domain/ProjectTest.java
@@ -0,0 +1,24 @@
+package keastudents.projectplanner.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectTest {
+
+    @Test
+    void addSubproject_successfully() {
+
+
+
+    }
+
+    @Test
+    void addSubproject_failed_to_added_object_not_subproject() {
+    //assertfail / assertexeption
+    }
+    @Test
+    void removeSubproject() {
+
+    }
+}
